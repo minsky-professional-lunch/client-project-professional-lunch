c6d4c038fb8a620c251a2c9a8516e38efabd6efe
Abdi <abdullahia0215@gmail.com>
2024-07-18 09:44:27 -0500

    added an action for clearing profile check status after logout

--- src/redux/reducers/profile.check.reducer.js
+++ src/redux/reducers/profile.check.reducer.js
@@ -5,6 +5,9 @@
 const profileCheck = (state = false, action) => {
   if (action.type === "SET_PROFILE_CHECK_RESULT") {
     return action.payload;
+  }
+  if (action.type === "CLEAR_PROFILE_CHECK") {
+    return state;
   } else {
     return state;
   }
