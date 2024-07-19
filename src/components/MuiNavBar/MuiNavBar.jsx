7f655514ab928b8e9d1388612f68c9c98e955b8d
Scott Schulte <scott.r.schulte@gmail.com>
2024-07-18 21:13:47 -0500

    Start of adding MuiNavBar

---
+++ src/components/MuiNavBar/MuiNavBar.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+
+const settings = ['Profile', 'Available Mentors', 'Logout']
+
+
+export default function MuiNavBar() {
+  return (
+    <div>
+
+    </div>
+  )
+}
