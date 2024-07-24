87ac0f15c30c3563a0676299994d84daa9e5d2b2
Scott Schulte <scott.r.schulte@gmail.com>
2024-07-24 15:37:18 -0500

    Co-authored-by: Amber Schaack <amberschaack@users.noreply.github.com>

--- src/components/Profile/EditProfileDialog.jsx
+++ src/components/Profile/EditProfileDialog.jsx
@@ -1,9 +1,219 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Avatar, Stack, Typography } from '@mui/joy';
+import Box from '@mui/joy/Box';
+import Button from '@mui/joy/Button';
+import ButtonGroup from '@mui/joy/ButtonGroup';
+import Badge from '@mui/joy/Badge';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import Container from '@mui/material/Container';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import Textarea from '@mui/joy/Textarea';
+import { TextField, Autocomplete } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+
+export default function EditProfileDialog({ open, closeEditProfile, profile}) {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  // const profile = useSelector((store) => store.profileDetails);
+  const schools = useSelector((store) => store.schoolsReducer);
+  const genders = useSelector((store) => store.gendersReducer);
+  const interestsStore = useSelector((store) => store.interestsReducer);
+  const profileAvailability = useSelector((store) => store.profileDetails.details.availability);
+  const days = useSelector((store) => store.dayReducer);
+  const times = useSelector((store) => store.timeReducer);
+
+  const [interests, setInterests] = useState(profile?.details?.interests);
+  const [availability, setAvailability] = useState(profile?.details?.availability);
+  const [editProfile, setEditProfile] = useState({
+    profile: {},
+    details: { availability: profileAvailability },
+  });
+  console.log('Profile', profile, editProfile);
 
 
-export default function EditProfileDialog() {
+  useEffect(() => {
+    setEditProfile(profile);
+  }, [profile]); // when profile in redux has finished loading, load into local state so we can edit it
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_PROFILE_DETAILS' });
+    dispatch({ type: 'FETCH_SCHOOLS' });
+    dispatch({ type: 'FETCH_GENDERS' });
+    dispatch({ type: 'FETCH_INTERESTS' });
+    dispatch({ type: 'FETCH_DAYS' });
+    dispatch({ type: 'FETCH_TIMES' });
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('clicked');
+    console.log('editProfile', editProfile);
+    dispatch({ type: 'EDIT_PROFILE', payload: editProfile });
+    closeEditProfile();
+  };
+
+  const handleInterestsChange = (event, newValue) => {
+    console.log('The New Value', newValue);
+    console.log('interests', interests);
+    if (newValue.length <= 5) {
+      setInterests(newValue);
+      setEditProfile({
+        ...editProfile,
+        details: {
+          ...editProfile.details,
+          interests: newValue,
+        },
+      });
+    }
+  };
+
+  const openWidget = () => {
+    !!window.cloudinary &&
+      window.cloudinary
+        .createUploadWidget(
+          {
+            sources: ['local', 'url', 'camera'],
+            cloudName: 'dz2bil44j',
+            uploadPreset: 'hl5wdxak',
+          },
+          (error, result) => {
+            if (!error && result && result.event === 'success') {
+              setEditProfile({
+                ...editProfile,
+                profile: {
+                  ...editProfile.profile,
+                  avatar: result.info.secure_url,
+                },
+              });
+            }
+          }
+        )
+        .open();
+  };
+
   return (
     <>
-    
+    <Dialog
+        open={open}
+        onClose={closeEditProfile}
+        PaperProps={{ component: 'form', onSubmit: handleSubmit }}
+        >
+        <DialogTitle>Edit Profile</DialogTitle>
+        <DialogContent>
+          <TextField
+              sx={{ mb: 1.5, mt: 1 }}
+              placeholder='Bio'
+              type='text'
+              id='bio'
+              label='Bio'
+              fullWidth
+              multiline
+              minRows={3}
+              value={editProfile?.profile?.bio}
+              className='form-control'
+              onChange={(event) =>
+                setEditProfile({
+                  ...editProfile,
+                  profile: { ...editProfile.profile, bio: event.target.value },
+                })
+              }
+            />
+            <TextField 
+            sx={{ mb: 1.5 }}
+            placeholder='Email'
+            type='text'
+            id='email'
+            name='email'
+            label='Email'
+            className='form-control'
+            value={editProfile?.profile?.email}
+            onChange={(event) =>
+              setEditProfile({
+                ...editProfile,
+                profile: {
+                  ...editProfile.profile,
+                  email: event.target.value,
+                },
+              })
+            }
+            
+            />
+             <TextField
+              placeholder='LinkedIn'
+              className='form-control'
+              type='text'
+              id='linkedin'
+              label='LinkedIn'
+              fullWidth
+              multiline
+              value={editProfile?.profile?.linkedin}
+              onChange={(e) =>
+                setEditProfile({
+                  ...editProfile,
+                  profile: {
+                    ...editProfile.profile,
+                    linkedin: e.target.value,
+                  },
+                })
+              }
+            />
+          <InputLabel>Gender</InputLabel>
+           <Select value={editProfile?.profile.gender}
+              onChange={(event) =>
+                setEditProfile({
+                  ...editProfile,
+                  profile: {
+                    ...editProfile.profile,
+                    gender: Number(event.target.value),
+                  },
+                })
+              }>
+              {genders.map((gender) => (
+                <MenuItem key={gender.id} value={gender.id}>
+                  {gender.gender}
+                </MenuItem>
+              ))}
+            </Select>
+            <InputLabel>Interests</InputLabel>
+            <Autocomplete
+            multiple
+            options={interestsStore}
+            value={interests}
+            getOptionLabel={(option) => option.interest}
+            disableCloseOnSelect
+            onChange={handleInterestsChange}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                variant='outlined'
+                label='Add Up To 5 Interests'
+                placeholder='Interests...'
+              />
+            )}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button
+            color='neutral'
+            variant='outlined'
+            type='button'
+            onClick={() => closeEditProfile()}
+          >
+            Cancel
+          </Button>
+          <Button type='submit' color='primary' variant='outlined'>Submit</Button>
+        </DialogActions>
+        </Dialog>
     </>
   )
 }
---
+++ src/components/Profile/Profile2.jsx
@@ -0,0 +1,163 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useScript } from '../../hooks/useScript';
+import React, { useEffect, useState } from 'react';
+import { Avatar, Stack, Typography } from '@mui/joy';
+import Badge from '@mui/joy/Badge';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import Box from '@mui/material/Box';
+import Button from '@mui/joy/Button';
+import ButtonGroup from '@mui/joy/ButtonGroup';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import CardOverflow from '@mui/joy/CardOverflow';
+import CardActions from '@mui/joy/CardActions';
+import Grid from '@mui/joy/Grid';
+import Textarea from '@mui/joy/Textarea';
+import Add from '@mui/icons-material/Add';
+import Tooltip from '@mui/joy/Tooltip';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
+import EmailIcon from '@mui/icons-material/Email';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Chip from '@mui/joy/Chip';
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+import EditProfileDialog from './EditProfileDialog';
+
+export default function Profile2() {
+  const dispatch = useDispatch();
+  const profile = useSelector((store) => store.profileDetails);
+  console.log('Profile Details', profile);
+  const schools = useSelector((store) => store.schoolsReducer);
+  const genders = useSelector((store) => store.gendersReducer);
+  const interestsStore = useSelector((store) => store.interestsReducer);
+  // const profileAvailability = useSelector(
+  //   (store) => store.profileDetails.details.availability
+  // );
+  const days = useSelector((store) => store.dayReducer);
+  const times = useSelector((store) => store.timeReducer);
+
+  const [editProfileIsOpen, setEditProfileIsOpen] = useState(false);
+  const closeEditProfile = () => setEditProfileIsOpen(false);
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_PROFILE_DETAILS' });
+    dispatch({ type: 'FETCH_SCHOOLS' });
+    dispatch({ type: 'FETCH_GENDERS' });
+    dispatch({ type: 'FETCH_INTERESTS' });
+    dispatch({ type: 'FETCH_DAYS' });
+    dispatch({ type: 'FETCH_TIMES' });
+  }, []);
+
+  const openWidget = () => {
+    !!window.cloudinary &&
+      window.cloudinary
+        .createUploadWidget(
+          {
+            sources: ['local', 'url', 'camera'],
+            cloudName: 'dz2bil44j',
+            uploadPreset: 'hl5wdxak',
+          },
+          (error, result) => {
+            if (!error && result && result.event === 'success') {
+              setEditProfile({
+                ...editProfile,
+                profile: {
+                  ...editProfile.profile,
+                  avatar: result.info.secure_url,
+                },
+              });
+            }
+          }
+        )
+        .open();
+  };
+
+  // note: if user does not have a profile yet, will this prevent them from editing their empty profile?
+  if (!profile.profile) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div className='container'>
+      <Grid container justifyContent="center">
+        <Box sx={{ maxHeight: '90vh' }}>
+        <Stack direction='column' spacing={1.5} alignItems='center'>
+        <Card
+          sx={{
+            width: '82vw',
+            maxWidth: '100%',
+            boxShadow: 'lg',
+          }}
+        >
+          <CardContent sx={{ alignItems: 'center', textAlign: 'center' }}>
+            <Typography sx={{ fontSize: '2rem', fontWeight: 'bold'}} level='h2'>
+              {profile?.profile?.first_name} {profile?.profile?.last_name}'s Profile
+            </Typography>
+            <Avatar src={profile?.profile?.avatar} sx={{ '--Avatar-size': '10rem', marginBottom: '3px' }}/>
+            <Stack direction='row' alignItems='center' spacing={1}>
+                {profile?.profile?.linkedin != null ? 
+                <Button component="a" href={profile?.profile?.linkedin} variant='plain' color='neutral'>
+                  <LinkedInIcon sx={{ fontSize: '2.5rem' }} />
+                </Button>
+              : 
+              <></>
+              }
+            </Stack>
+          </CardContent>
+        </Card>
+        <Card sx={{
+            width: '82vw',
+            maxWidth: '100%',
+            boxShadow: 'lg',
+          }}>
+            <Stack direction='column'>
+              <Typography level='h3'>About Me</Typography>
+              <Typography sx={{ fontSize: '1.3rem' }}>{profile?.profile?.bio}</Typography>
+            </Stack>
+        </Card>
+        <Card sx={{
+        width: '82vw',
+        maxWidth: '100%',
+        boxShadow: 'lg'
+          }}>
+            <Stack direction='column'>
+              <Typography level='h3'>Interests</Typography>
+              <Stack direction='row' spacing={1} flexWrap='wrap' useFlexGap>
+              {profile?.details?.interests?.map((interest) => (
+                  <Chip sx={{ fontSize: '1.3rem', marginTop: '10px'}}>{interest.interest}</Chip>
+                ))}
+                </Stack>
+            </Stack>
+        </Card>
+        <Card sx={{
+            width: '82vw',
+            maxWidth: '100%',
+            boxShadow: 'lg'
+          }}>
+            <Stack direction='column'>
+              <Typography level='h3'>Availability</Typography>
+              <Stack direction='row' spacing={1} flexWrap='wrap' useFlexGap>
+              {profile?.details?.availability?.map((avail) => (
+                  <Chip sx={{ fontSize: '1.3rem', marginTop: '10px' }}>
+                    {avail.day} @ {avail.time}
+                  </Chip>
+                ))}
+                </Stack>
+            </Stack>
+        </Card>
+            <Button
+                  size='lg'
+                  color='neutral'
+                  variant='outlined'
+                  sx={{ ml: '4px', alignSelf: 'center', fontWeight: 600 }}
+                  onClick={() => setEditProfileIsOpen(true)}
+                >
+                  Edit Profile
+                </Button>
+        </Stack>
+        </Box>
+        </Grid>
+                <EditProfileDialog open={editProfileIsOpen} closeEditProfile={closeEditProfile} profile={profile}/>
+    </div>
+  );
+}
