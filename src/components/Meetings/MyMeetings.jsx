a6488ea1853c61c520c8649ad1e069e3f16bb65c
Amber Schaack <aschaack9@gmail.com>
2024-07-25 15:39:46 -0500

    archive completed meetings

--- src/components/Meetings/MyMeetings.jsx
+++ src/components/Meetings/MyMeetings.jsx
@@ -2,16 +2,25 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import MeetingItem from './MeetingItem';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { Typography } from '@mui/joy';
 
 export default function MyMeetings() {
     const meetings = useSelector(store => store.meetings);
+    const upcomingMeetings = meetings.filter(meeting => meeting.meeting_status != 'completed');
+    const archiveMeetings = meetings.filter(meeting => meeting.meeting_status === 'completed');
+    console.log('archive', archiveMeetings);
+    console.log('upcoming meetings', upcomingMeetings);
     const user = useSelector(store => store.user);
     console.log('Meetings', meetings);
 
     return (
         <div className="container">
         <h1 align='center'>My Meetings</h1>
-        {meetings.map((meeting) => (
+        {upcomingMeetings.map((meeting) => (
                 <MeetingItem key={meeting.meeting_id} meeting={meeting} />
         ))}
         {meetings.length === 0 ? 
@@ -24,6 +33,25 @@ export default function MyMeetings() {
                 </>
             }
             </>
+        :
+        <></>
+        }
+        {archiveMeetings.length > 0 ? 
+        <>
+        <Accordion sx={{ marginTop: '50px'}}>
+            <AccordionSummary
+            expandIcon={<ArrowDropDownIcon />}
+            aria-controls='panel1-content'
+            id='panel1-header'>
+                <Typography>Archived Meetings</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                {archiveMeetings.map(meeting => (
+                    <MeetingItem meeting={meeting} key={meeting.meeting_id} />
+                ))}
+            </AccordionDetails>
+        </Accordion>
+        </>
         :
         <></>
         }
